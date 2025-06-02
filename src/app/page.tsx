986ff85ae5b52906627529a62a98e4d1c0005fe9
alessa_kessyla <alessakessyla99@gmail.com>
2025-06-02 20:50:46 -0300

    atividade01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Wellcome from "@/components/wellcome";
+import NavBar from "@/components/NavBar";
 
-export default function Home() {
-  return (
-    <div className="">
-      <Wellcome/>
-    </div>
-  );
-}
+export default function Home(){
+  return(
+  <div className="">
+
+    <NavBar links={
+      [
+        {label:'Inicio', href:'/'},
+        {label:'Sobre Nós', href:'/about'},
+        {label:'Produtos', href:'/product'}
+        ]
+      } /> 
+  </div>
+  )
+};
+    
